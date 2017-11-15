c2ea77b435f4da803302c5836d05fd57f5f7bf53
Battaile Fauber <battaile@gmail.com>
2017-11-15 14:46:27 -0500

    shop component

--- src/GoogleShop/index.js
+++ src/GoogleShop/index.js
@@ -1,5 +1,28 @@
 import React from 'react'
 
-const GoogleShop = () => <div>google tool</div>
+const GoogleShop = () => (
+  <div className="container" style={{width:'50%'}}>
+    <div className="panel panel-default">
+      <div className="panel-heading">Currently Listed</div>
+      <div className="panel-body">
+        <div className="row">
+          <div className="col-sm-5">
+            <strong>Select a Vendor</strong><br/>
+            <select className="formControl" style={{width:'13em'}}>
+              <option>--</option>
+            </select>
+          </div>
+          <div className="col-sm-2" style={{ paddingTop:'.5em' }}><strong>OR</strong></div>
+          <div className="col-sm-5">
+            <strong>Select a Site</strong><br/>
+            <select className="formControl" style={{width:'13em'}}>
+              <option>--</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
 
 export default GoogleShop;
