3d5e5c565b575ef8ec05107ed9fa98d6b36d0d8c
Battaile Fauber <battaile@gmail.com>
2017-11-16 10:50:54 -0500

    select site

--- src/GoogleShop/GoogleShopPanel.js
+++ src/GoogleShop/GoogleShopPanel.js
@@ -3,14 +3,24 @@ import React, {PropTypes} from 'react'
 const VendorDropdown = ({vendors}) => (
   <select className="formControl" style={{width:'13em'}}>
     <option>--</option>
-    {vendors && vendors.map((v)=> <option key={v.id} value={v.id}>{v.vendor_name}</option> )}
+    {vendors && vendors.map((v)=> <option key={v.vendor_name} value={v.id}>{v.vendor_name}</option> )}
   </select>
 )
 VendorDropdown.propTypes = {
   vendors: PropTypes.array
 }
 
-const GoogleShopPanel = ({vendors}) => (
+const StoreDropdown = ({stores}) => (
+  <select className="formControl" style={{width:'13em'}}>
+    <option>--</option>
+    {stores && stores.map((s)=> <option key={s.id} value={s.id}>{s.store_name}</option> )}
+  </select>
+)
+StoreDropdown.propTypes = {
+  stores: PropTypes.array
+}
+
+const GoogleShopPanel = ({vendors, stores}) => (
   <div className="container" style={{width:'50%'}}>
   <div className="panel panel-default">
     <div className="panel-heading">Currently Listed</div>
@@ -18,15 +28,12 @@ const GoogleShopPanel = ({vendors}) => (
       <div className="row">
         <div className="col-md-5">
           <strong>Select a Vendor</strong><br/>
-          { <VendorDropdown vendors={vendors} /> }
-          
+          { <VendorDropdown vendors={vendors} /> }          
         </div>
         <div className="col-md-2" style={{ paddingTop:'.5em' }}><strong>OR</strong></div>
         <div className="col-md-5">
           <strong>Select a Site</strong><br/>
-          <select className="formControl" style={{width:'13em'}}>
-            <option>--</option>
-          </select>
+          { <StoreDropdown stores={stores} /> }
         </div>
       </div>
     </div>
@@ -35,6 +42,7 @@ const GoogleShopPanel = ({vendors}) => (
 )
 
 GoogleShopPanel.propTypes = {
-  vendors: PropTypes.array
+  vendors: PropTypes.array,
+  stores: PropTypes.array
 }
 export default GoogleShopPanel
--- src/GoogleShop/index.js
+++ src/GoogleShop/index.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import GoogleShopPanel from './GoogleShopPanel'
-import Loadable from 'react-loading-overlay'
+import React from "react";
+import GoogleShopPanel from "./GoogleShopPanel";
+import Loadable from "react-loading-overlay";
 
 class GoogleShop extends React.Component {
-  constructor(){
-    super()
-    this.state = { 
+  constructor() {
+    super();
+    this.state = {
       vendors: null
-    }
+    };
   }
 
-  componentDidMount(){
-    GetVendorList((vendors) => this.setState({vendors}))
+  componentDidMount() {
+    GetVendorList(vendors => this.setState({ vendors }));
+    GetStoreLookups(stores => this.setState({ stores }));
   }
 
   render() {
-    const loading = !this.state.vendors;
-    const spinnerText = 'Loading vendors...';
-    console.log('loading=' + loading)
-    return  (
+    const loading = !this.state.vendors || !this.state.stores;
+
+    let spinnerText;
+    if (!this.state.vendors) {
+      spinnerText = "Loading vendors...";
+    } else if (!this.state.stores) {
+      spinnerText = "Loading stores...";
+    }
+
+    return (
       <Loadable active={loading} spinner text={spinnerText}>
-        <GoogleShopPanel vendors={this.state.vendors} />
+        <GoogleShopPanel vendors={this.state.vendors} stores={this.state.stores} />
       </Loadable>
-    )
+    );
   }
 }
 
-export default GoogleShop
+export default GoogleShop;
