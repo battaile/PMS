c77da31b2260088d16465de7f0b894e26a8eb3a5
Battaile Fauber <battaile@gmail.com>
2017-11-28 08:46:26 -0500

    fix items

--- src/GoogleShop/Items.js
+++ src/GoogleShop/Items.js
@@ -1,9 +1,11 @@
 import React, {PropTypes} from 'react';
 
-const Items = ({filter}) => {
-  <div> {filter.vendor}: {filter.status} </div>
-}
+const Items = ({filter}) => (
+  <div> {filter.vendor.name} ({filter.vendor.id}-{filter.vendor.store_id}) : {filter.status} </div>
+)
 
 Items.propTypes = {
   filter: PropTypes.object.isRequired
 }
+
+export default Items;
