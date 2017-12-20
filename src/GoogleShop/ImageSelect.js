afd67051f62c8cc33236b1b17971c0ee35109bf1
Battaile Fauber <battaile@gmail.com>
2017-12-20 10:11:57 -0500

    image select layout

--- src/GoogleShop/ImageSelect.js
+++ src/GoogleShop/ImageSelect.js
@@ -3,21 +3,61 @@ import LazyLoad from "react-lazyload";
 import PropTypes from "prop-types";
 
 const Image = ({ imageLink }) => (
-  
- <div>
-     <label htmlFor={imageLink.name}>{imageLink.name}</label>
-     <img src={imageLink.url} id={imageLink.name} />
-  </div>
+  <span>
+    <label htmlFor={imageLink.name}>{imageLink.name}</label>
+    <LazyLoad height={100}>
+      <img
+        src={imageLink.url}
+        id={imageLink.name}
+        style={{ height: 100, width: "auto" }}
+        className="form-control"
+      />
+    </LazyLoad>
+  </span>
 );
 Image.propTypes = {
   imageLink: PropTypes.object.isRequired
 };
 
-const ImageSelect = ({ imageLinks }) => (
-  <div className="container">test
-    {imageLinks.map(i => <Image key={i.url} imageLink={i} />)}
-  </div>
-);
+const ImageSelect = ({ imageLinks }) => {
+  let col1 = [], col2 = [], col3 = [], col4 = [];
+
+  for (let i = 0; i < imageLinks.length; i++) {
+    col1.push(imageLinks[i]);
+    i++;
+    if (i >= imageLinks.length) {break;}
+
+    col2.push(imageLinks[i]);
+    i++;
+    if (i >= imageLinks.length) {break;}
+
+    col3.push(imageLinks[i]);
+    i++;
+    if (i >= imageLinks.length) {break;}
+    
+    col4.push(imageLinks[i]);
+  }
+
+  return (
+      <div className="row">
+        <div className="col-sm-3">
+          {col1.map(i => <Image key={i.url} imageLink={i} />)}
+        </div>
+
+        <div className="col-sm-3">
+          {col2.map(i => <Image key={i.url} imageLink={i} />)}
+        </div>
+
+        <div className="col-sm-3">
+          {col3.map(i => <Image key={i.url} imageLink={i} />)}
+        </div>
+
+        <div className="col-sm-3">
+          {col4.map(i => <Image key={i.url} imageLink={i} />)}
+        </div>        
+      </div>
+  );
+};
 
 ImageSelect.propTypes = {
   imageLinks: PropTypes.array.isRequired
